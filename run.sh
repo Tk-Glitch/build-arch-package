e8c6fee82a380a9a6fdb772b6acc8f7c28f5ec72
Etienne Juvigny <Ti3noU@gmail.com>
2020-09-10 19:16:24 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -33,6 +33,7 @@ echo -e '_NOINITIALPROMPT="false"' > /home/user/.config/frogminer/wine-tkg.cfg
 cd "$PKGBUILD_DIR"
 chown -R user ./*
 chmod +w -R *
+chmod +w "$SRCDEST"
 
 # Build the package
 multilib-build -- -U user
